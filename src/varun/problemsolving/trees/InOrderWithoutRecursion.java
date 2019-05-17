022afba69aca2d84e3129e5cc40e7f2e4773af82
Varun A M <varun.am@olacabs.com>
2019-05-17 13:52:23 +0530

    added problems

--- src/varun/problemsolving/trees/InOrderWithoutRecursion.java
+++ src/varun/problemsolving/trees/InOrderWithoutRecursion.java
@@ -2,6 +2,22 @@ package varun.problemsolving.trees;
 
 import java.util.Stack;
 
+/**
+ * Using Stack is the obvious way to traverse tree without recursion. Below is an algorithm for traversing binary tree using stack. See this for step wise step execution of the algorithm.
+ *
+ * 1) Create an empty stack S.
+ * 2) Initialize current node as root
+ * 3) Push the current node to S and set current = current->left until current is NULL
+ * 4) If current is NULL and stack is not empty then
+ *      a) Pop the top item from stack.
+ *      b) Print the popped item, set current = popped_item->right
+ *      c) Go to step 3.
+ * 5) If current is NULL and stack is empty then we are done.
+ *
+ * PROBLEM AND SOLUTION:
+ * https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion/
+ *
+ */
 public class InOrderWithoutRecursion {
 
     public static void main(String[] args) {
